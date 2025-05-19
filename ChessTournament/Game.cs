02824e0caf90c6393f1eff9210ef785b660429fa
gabandr <andriy.gabidullin@gmail.com>
2025-05-19 12:18:16 +0300

    Game base structure

--- ChessTournament/Game.cs
+++ ChessTournament/Game.cs
@@ -8,7 +8,14 @@ namespace ChessTournament
 {
     public class Game
     {
-
+        public Player playerW;
+        public Player playerB;
+        public float Result;
+        Game(Player playerw, Player playerb, float result) {
+            playerB = playerb;
+            playerW = playerw;
+            Result = result;
+        }
 
     }
 }
