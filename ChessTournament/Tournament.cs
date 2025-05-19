bd32c974818b3a759e3f21680a445af486c1aee5
gabandr <andriy.gabidullin@gmail.com>
2025-05-19 16:49:39 +0300

    Added AddGame to Tournament

--- ChessTournament/Tournament.cs
+++ ChessTournament/Tournament.cs
@@ -6,9 +6,29 @@ using System.Threading.Tasks;
 
 namespace ChessTournament
 {
-    internal class Tournament
+    public class Tournament
     {
-        Game[][] games;
-        int AmountOfRounds;
+        public List<List<Game>> Games;
+        public int AmountOfRounds;
+        public int Year;
+        public Tournament(int amountOfRounds, int year)
+        {
+            AmountOfRounds = amountOfRounds;
+            Year = year;
+            Games = new List<List<Game>>(amountOfRounds);
+        }
+        public void AddGame(Game game, int Round)
+        {
+            if (Round >= 0 && Round < AmountOfRounds)
+            {
+                if (Games.Count <= Round)
+                {
+                    Games.Add(new List<Game>());
+                }
+                Games[Round].Add(game);
+            }
+
+        }
+
     }
 }
